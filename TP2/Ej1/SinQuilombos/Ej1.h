5309584dcb623027ceb7bc2c22938617f54c1b22
Pedro Tamborindeguy <pftambo@gmail.com>
2017-10-08 20:30:04 -0300

    Ej1 limpio

---
+++ TP2/Ej1/SinQuilombos/Ej1.h
@@ -0,0 +1,14 @@
+#ifndef ALGO3_EJ1_H
+#define ALGO3_EJ1_H
+
+#include <vector>
+#include <cstdio>
+#include <iostream>
+#include <list>
+#include <stack>
+#include <queue>
+#include <climits>
+
+using namespace std;
+
+#endif //ALGO3_EJ1_H
